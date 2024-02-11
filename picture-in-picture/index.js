c6458ecb50e111cb3542c8994a661dbfe1d5048b
Wesley Beck <74019873+wizbeck21@users.noreply.github.com>
2024-02-10 18:13:24 -0700

    picture-in-picture project completed

---
+++ picture-in-picture/index.js
@@ -0,0 +1,26 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element, then play
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Errors
+    console.warn("Whoops, error here: ", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  button.disabled = true;
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
